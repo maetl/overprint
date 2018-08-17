bfb4b2954d2cca5b4f58ff8068f440f7faa6eb31
Mark Rickerby <me@maetl.net>
2018-08-17 16:29:38 +1200

    Upgrading Rollup build script

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,19 @@
-export default {
-  entry: 'src/overprint.js',
-  format: 'iife',
-  moduleName: 'overprint',
-  dest: 'overprint.js'
-};
+import pkg from './package.json';
+
+export default [
+	{
+		input: 'overprint/overprint.js',
+		output: {
+      name: 'overprint',
+			file: pkg.browser,
+			format: 'iife'
+		}
+	},
+	{
+		input: 'overprint/overprint.js',
+		output: [
+			{ file: pkg.main, format: 'cjs' },
+			{ file: pkg.module, format: 'es' }
+		]
+	}
+];
