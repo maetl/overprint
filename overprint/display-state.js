977f6b9526e2b1564fbc3df2380a4159456f9c46
Mark Rickerby <me@maetl.net>
2019-06-19 23:39:56 +1200

    Convert `DisplayState` to modern ES class syntax

--- overprint/display-state.js
+++ overprint/display-state.js
@@ -1,78 +1,80 @@
 function fillArray2D(width, height, fill) {
-  var list = new Array(width);
-  for (var col=0; col<width; col++) {
+  const list = new Array(width);
+  for (let col=0; col<width; col++) {
     list[col] = new Array(height);
-    for (var row=0; row<height; row++) {
+    for (let row=0; row<height; row++) {
       list[col][row] = fill;
     }
   }
   return list;
 }
 
-const DisplayState = function(width, height, cell) {
-	this._width = width;
-	this._height = height;
-	this._emptyCell = cell;
+class DisplayState {
+  constructor(width, height, emptyCell) {
+    this._width = width;
+  	this._height = height;
+  	this._emptyCell = emptyCell;
 
-	this._renderedCells = fillArray2D(width, height, cell);
-	this._updatedCells = fillArray2D(width, height, cell);
+    this._renderedCells = fillArray2D(width, height, emptyCell);
+  	this._updatedCells = fillArray2D(width, height, emptyCell);
 
-	this._dirty = true;
-}
-
-DisplayState.prototype.setCharacter = function(x, y, character) {
-  if (x < 0) return;
-  if (x >= this._width) return;
-  if (y < 0) return;
-  if (y >= this._height) return;
-
-  if (!character) return;
-
-  if (this._renderedCells[x][y].character != character) {
-    const foregroundColor = this._updatedCells[x][y].foregroundColor || this._renderedCells[x][y].foregroundColor
-    const backgroundColor = this._updatedCells[x][y].backgroundColor || this._renderedCells[x][y].backgroundColor
-    this._updatedCells[x][y] = {
-      character,
-      foregroundColor,
-      backgroundColor
-    };
-    this._dirty = true;
-  } else {
-    this._updatedCells[x][y] = null;
+  	this._dirty = true;
   }
-}
 
-DisplayState.prototype.setCell = function(x, y, cell) {
-	if (x < 0) return;
-	if (x >= this._width) return;
-	if (y < 0) return;
-	if (y >= this._height) return;
+  setCharacter(x, y, character) {
+    if (x < 0) return;
+    if (x >= this._width) return;
+    if (y < 0) return;
+    if (y >= this._height) return;
 
-	if (!cell) cell = this._emptyCell;
+    if (!character) return;
 
-	if (this._renderedCells[x][y] !== cell) {
-		this._updatedCells[x][y] = cell;
-		this._dirty = true;
-	} else {
-		this._updatedCells[x][y] = null;
-	}
-}
+    if (this._renderedCells[x][y].character != character) {
+      const foregroundColor = this._updatedCells[x][y].foregroundColor || this._renderedCells[x][y].foregroundColor
+      const backgroundColor = this._updatedCells[x][y].backgroundColor || this._renderedCells[x][y].backgroundColor
+      this._updatedCells[x][y] = {
+        character,
+        foregroundColor,
+        backgroundColor
+      };
+      this._dirty = true;
+    } else {
+      this._updatedCells[x][y] = null;
+    }
+  }
 
-DisplayState.prototype.render = function(callback) {
-	if (!this._dirty) return;
+  setCell(x, y, cell) {
+  	if (x < 0) return;
+  	if (x >= this._width) return;
+  	if (y < 0) return;
+  	if (y >= this._height) return;
 
-	for (var col=0; col<this._width; col++) {
-		for (var row=0; row<this._height; row++) {
-			var cell = this._updatedCells[col][row];
+  	if (!cell) cell = this._emptyCell;
 
-			if (cell == null) continue;
+  	if (this._renderedCells[x][y] !== cell) {
+  		this._updatedCells[x][y] = cell;
+  		this._dirty = true;
+  	} else {
+  		this._updatedCells[x][y] = null;
+  	}
+  }
 
-			callback(col, row, cell);
+  render(callback) {
+  	if (!this._dirty) return;
 
-			this._renderedCells[col][row] = cell;
-			this._updatedCells[col][row] = null;
-		}
-	}
+  	for (var col=0; col<this._width; col++) {
+  		for (var row=0; row<this._height; row++) {
+  			var cell = this._updatedCells[col][row];
+
+  			if (cell == null) continue;
+
+  			callback(col, row, cell);
+
+  			this._renderedCells[col][row] = cell;
+  			this._updatedCells[col][row] = null;
+  		}
+  	}
+  }
 }
 
 export default DisplayState;
