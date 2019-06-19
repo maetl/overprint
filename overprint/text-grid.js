0e8b9d1266dcebe83d6986e9bb9f016694652300
Mark Rickerby <me@maetl.net>
2019-06-19 23:43:17 +1200

    Replace `var` with `const` and `let` in code from legacy `Terminal`

--- overprint/text-grid.js
+++ overprint/text-grid.js
@@ -40,8 +40,8 @@ class TextGrid {
   		if (!this._canvas.style.width) this._canvas.style.width = 640;
   		if (!this._canvas.style.height) this._canvas.style.height = 480;
 
-  		var elementWidth = parseInt(this._canvas.style.width, 10)  * this._ratio;
-  		var elementHeight = parseInt(this._canvas.style.height, 10) * this._ratio;
+  		const elementWidth = parseInt(this._canvas.style.width, 10)  * this._ratio;
+  		const elementHeight = parseInt(this._canvas.style.height, 10) * this._ratio;
 
   		// Calculates cell width and height based on the containing element
   		this._cellWidth = Math.floor(elementWidth / this._width);
@@ -57,25 +57,22 @@ class TextGrid {
 
   	} else {
   		// Measure text width from canvas context
-  		var textMeasure = this._context.measureText("M");
+  		const textMeasure = this._context.measureText("M");
 
   		// Canvas measure text with direct multiple of font size
-  		var textWidth = Math.ceil(textMeasure.width);
-  		var textHeight = this._font.size;
+  		let textWidth = Math.ceil(textMeasure.width);
+  		let textHeight = this._font.size;
 
   		// Force square aspect ratio
   		if (this._squared) {
   			textWidth = textHeight = Math.max(textWidth, textHeight);
   		}
 
-  		var glyphWidth = textWidth;
-  		var glyphHeight = textHeight;
+  		this._cellWidth = textWidth * this._ratio;
+  		this._cellHeight = textHeight * this._ratio;
 
-  		this._cellWidth = glyphWidth * this._ratio;
-  		this._cellHeight = glyphHeight * this._ratio;
-
-  		var drawWidth = glyphWidth * this._width;
-  		var drawHeight = glyphHeight * this._height;
+  		const drawWidth = textWidth * this._width;
+  		const drawHeight = textHeight * this._height;
 
   		this._canvas.width = drawWidth * this._ratio;
   		this._canvas.height = drawHeight * this._ratio;
@@ -95,8 +92,8 @@ class TextGrid {
   }
 
   fill(cell) {
-    for (var col=0; col<this._width; col++) {
-      for (var row=0; row<this._height; row++) {
+    for (let col=0; col<this._width; col++) {
+      for (let row=0; row<this._height; row++) {
         this._display.setCell(col, row, cell);
       }
     }
@@ -112,8 +109,8 @@ class TextGrid {
 
   render() {
     this._display.render((x, y, cell) => {
-      var cellWidth = this._cellWidth;
-      var cellHeight = this._cellHeight;
+      const cellWidth = this._cellWidth;
+      const cellHeight = this._cellHeight;
 
       this._context.fillStyle = cell.backgroundColor;
       this._context.fillRect(x * cellWidth, y * cellHeight, cellWidth, cellHeight);
@@ -122,8 +119,8 @@ class TextGrid {
 
       this._context.fillStyle = cell.foregroundColor;
 
-      var xPos = (x * cellWidth) + cellWidth / 2;
-      var yPos = (y * cellHeight) + cellHeight / 2;
+      const xPos = (x * cellWidth) + cellWidth / 2;
+      const yPos = (y * cellHeight) + cellHeight / 2;
       this._context.fillText(cell.character, xPos, yPos);
     });
   }
